86a16582d4923453a8a3928ec95d26e51cf62736
David Michael Zalk <dzknj@outlook.com>
2016-04-05 22:37:59 -0700

    added gitignore readme package.json gulpfil index.js

---
+++ davez/README.md
@@ -0,0 +1,10 @@
+This is the readme file for my greet program, which is called modname. It says
+hello to jersey dave, (myself) on the command line when run and when run with
+an argument after the filename modname.js when run with node, it takes that
+argument and below the part where it says hello to jersey dave, it says hello
+to the argument which should be a name but can be anything. It does this on a
+new line.
+I have a gulpfile that runs eslint and also the mocha test. It is also set up
+so that when you run gulp, it remains open and anytime any of the files are
+edited in relation to the tester or the linter, it automatically lints and
+tests again. I believe that this is a watch task.
---
+++ davez/gulpfile.js
@@ -0,0 +1,41 @@
+const gulp = require('gulp');
+const eslint = require('gulp-eslint');
+const mocha = require('gulp-mocha');
+
+var files = ['index.js', 'lib/**/*.js', 'gulpfile.js'];
+
+gulp.task('mochatest', function() {
+  return gulp.src('test/*.js')
+    .pipe(mocha());
+});
+
+gulp.task('lint:test', function() {
+  return gulp.src('./test/**/*test.js')
+    .pipe(eslint({
+      rules: {
+        'indent': ['error', 2]
+      },
+      envs: [
+        'mocha'
+      ]
+    }))
+    .pipe(eslint.format());
+});
+
+gulp.task('lint:notest', function() {
+  return gulp.src(files)
+    .pipe(eslint({
+      rules: {
+        'indent': ['error', 2]
+      }
+    }))
+    .pipe(eslint.format());
+});
+
+gulp.task('lint', ['lint:notest', 'lint:test']);
+
+gulp.task('default', ['lint','mochatest'], function() {
+  gulp.watch(['test/*.js', files, './test/**/*test.js'], function() {
+    gulp.run('lint', 'mochatest');
+  });
+});
